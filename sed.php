273d7f6d6c0f93951b6ccd2150a4f64be041be7c
Brianmartinezsebas <59107116+Brianmartinezsebas@users.noreply.github.com>
2022-02-07 23:41:50 -0300

    Update sed.php

--- sed.php
+++ sed.php
@@ -3,16 +3,16 @@
 	// Estas son las claves para encriptar y desencriptar
 	// puedes generarlas en el siguiente link:
 	// https://www.lastpass.com/es/features/password-generator
-	// para tener seguro tus datos se recomienda un minimo de 16 caracteres en las 2 claves
-	// recuerda guardan bien las claves ya que si las pierdes no podras desencriptar los datos
+	// para tener seguro tus datos se recomienda un mínimo de 16 caracteres en las 2 claves
+	// recuerda guardan bien las claves, ya que si las pierdes no podrás desencriptar los datos
 
-	      private $METHOD = "AES-256-CBC"; // este es el metodo de encriptacion
-	      private $SECRET_KEY = "yZMX%5uNBYj^EehuGvH#l5%zxBEhH0&2"; // esta clave puede contener mayusculas, minusculas, numeros y simbolos
-	      private $SECRET_IV = "23006541971613219213028680229858"; // en esa solo se pueden numeros
+	      private $METHOD = "AES-256-CBC"; // Este es el método de encriptación
+	      private $SECRET_KEY = "yZMX%5uNBYj^EehuGvH#l5%zxBEhH0&2"; // Esta clave puede contener mayúsculas, minúsculas, números y símbolos
+	      private $SECRET_IV = "23006541971613219213028680229858"; // En esa solo se pueden números
 	      private $output,$iv,$key;
 
 		private function fol(){
-		    $this->iv = substr(hash('sha256', $this->SECRET_IV), 0, 16); // el numero 16 significa las capas de encriptado, entre mas alto es el numero mas seguridad, pero tardara mas en encriptar/desencriptar
+		    $this->iv = substr(hash('sha256', $this->SECRET_IV), 0, 16); // El número 16 significa las capas de encriptado, entre más alto es el número más seguridad, pero tardará más en encriptar/desencriptar
 		    $this->key = hash('sha256', $this->SECRET_KEY);
 		}
 		public function encryption($string){
