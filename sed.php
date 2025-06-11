d5fa7e0a684eae292eadd5c8d802caf926ce0258
Zerotet (Xer0) <brian.sebasmart@gmail.com>
2025-06-11 01:34:01 -0300

    Update sed.php

--- sed.php
+++ sed.php
@@ -1,29 +1,22 @@
 <?php
-	class SED {
-	// Estas son las claves para encriptar y desencriptar
-	// puedes generarlas en el siguiente link:
-	// https://www.lastpass.com/es/features/password-generator
-	// para tener seguro tus datos se recomienda un mínimo de 16 caracteres en las 2 claves
-	// recuerda guardan bien las claves, ya que si las pierdes no podrás desencriptar los datos
-
-	      private $METHOD = "AES-256-CBC"; // Este es el método de encriptación
-	      private $SECRET_KEY = "yZMX%5uNBYj^EehuGvH#l5%zxBEhH0&2"; // Esta clave puede contener mayúsculas, minúsculas, números y símbolos
-	      private $SECRET_IV = "23006541971613219213028680229858"; // En esa solo se pueden números
-	      private $output,$iv,$key;
-
-		private function fol(){
-		    $this->iv = substr(hash('sha256', $this->SECRET_IV), 0, 16); // El número 16 significa las capas de encriptado, entre más alto es el número más seguridad, pero tardará más en encriptar/desencriptar
-		    $this->key = hash('sha256', $this->SECRET_KEY);
-		}
-		public function encryption($string){
-		    $this->fol();
-		    $this->output = base64_encode(openssl_encrypt($string, $this->METHOD, $this->key, 0, $this->iv));
-		    return $this->output;
-		}
-		public function decryption($string){
-		    $this->fol();
-		    $this->output = openssl_decrypt(base64_decode($string), $this->METHOD, $this->key, 0, $this->iv);
-		    return $this->output;
-		}
+  	class SED {
+	    private $method = "AES-256-CBC"; // Este es el método de encriptación
+	    private $secret_key = "yZMX%5uNBYj^EehuGvH#l5%zxBEhH0&2"; // Esta clave puede contener mayúsculas, minúsculas, números y símbolos
+	
+	    public function encryption($string){
+	        $key = hash('sha256', $this->secret_key);
+	        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($this->method));
+	        $encrypted = openssl_encrypt($string, $this->method, $key, 0, $iv);
+	        return base64_encode($iv . $encrypted);
 	    }
-  
+	
+	    public function decryption($string){
+	        $data = base64_decode($string);
+	        $iv_length = openssl_cipher_iv_length($this->method);
+	        $iv = substr($data, 0, $iv_length);
+	        $encrypted = substr($data, $iv_length);
+	        $key = hash('sha256', $this->secret_key);
+	        return openssl_decrypt($encrypted, $this->method, $key, 0, $iv);
+	    }
+	}
+
